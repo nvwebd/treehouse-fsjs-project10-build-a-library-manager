a41aa78bcbd6f51e2b25843fdb8f1235bb46097b
Nejc Vukovic <nejc.vukovic@nvwebd.com>
2019-01-20 00:59:35 +0100

    add seeder files

---
+++ src/db/seeders/20190119235432-patrons.js
@@ -0,0 +1,52 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "patrons",
+      [
+        {
+          id: 1,
+          first_name: "Andrew",
+          last_name: "Chalkley",
+          address: "1234 NE 20st St",
+          email: "andrew.chalkley@teamtreehouse.com",
+          library_id: "MCL1001",
+          zip_code: 90210
+        },
+        {
+          id: 2,
+          first_name: "Dave",
+          last_name: "McFarland",
+          address: "5252 NW 2nd St",
+          email: "dave.mcfarland@teamtreehouse.com",
+          library_id: "MCL1010",
+          zip_code: 90210
+        },
+        {
+          id: 3,
+          first_name: "Alena",
+          last_name: "Holligan",
+          address: "1404 SW 101st St",
+          email: "alena.holligan@teamtreehouse.com",
+          library_id: "MCL1100",
+          zip_code: 91210
+        },
+        {
+          id: 4,
+          first_name: "Michael",
+          last_name: "Poley",
+          address: "7070 NE 10th Ave",
+          email: "michael.poley@teamtreehouse.com",
+          library_id: "MCL1011",
+          zip_code: 91310
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("patrons", null, {});
+  }
+};
---
+++ src/db/seeders/20190119235441-loans.js
@@ -0,0 +1,56 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "loans",
+      [
+        {
+          id: 1,
+          book_id: 15,
+          patron_id: 2,
+          loaned_on: "2015-12-10",
+          return_by: "2020-10-20",
+          returned_on: null
+        },
+        {
+          id: 2,
+          book_id: 4,
+          patron_id: 1,
+          loaned_on: "2015-12-11",
+          return_by: "2015-12-18",
+          returned_on: null
+        },
+        {
+          id: 3,
+          book_id: 8,
+          patron_id: 1,
+          loaned_on: "2015-12-12",
+          return_by: "2015-12-19",
+          returned_on: null
+        },
+        {
+          id: 4,
+          book_id: 9,
+          patron_id: 3,
+          loaned_on: "2015-12-13",
+          return_by: "2015-12-20",
+          returned_on: null
+        },
+        {
+          id: 5,
+          book_id: 11,
+          patron_id: 4,
+          loaned_on: "2015-12-13",
+          return_by: "2015-12-20",
+          returned_on: "2015-12-17"
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("loans", null, {});
+  }
+};
