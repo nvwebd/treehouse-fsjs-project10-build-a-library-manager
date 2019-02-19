eeac0fbb21415d1d9040f6481e4ef8a768685f87
Nejc Vukovic <nejc.vukovic@nvwebd.com>
2019-02-19 01:30:58 +0100

    update: add all needed routes

--- src/routes/patrons.js
+++ src/routes/patrons.js
@@ -1,19 +1,96 @@
 const express = require("express");
 const router = express.Router();
-const Patrons = require("../db/models").patrons;
+const models = require("./../db/models");
+const pagination = require("./../utils/pagination");
+
+const formErrorCreator = errorArray => {
+  const errors = {};
+
+  errorArray.errors.map(
+    error => (errors[error.path] = { message: error.message })
+  );
+
+  return errors;
+};
 
 // define the home page route
-router.get("/", (req, res) => {
-  Patrons.findAll({}).then(instances => {
-    res.render("pages/patrons/all_patrons", {
-      title: "Patrons",
-      patrons: instances
-    });
+router.get("/", async (req, res) => {
+  const patrons = await models.patrons.findAndCountAll({
+    offset: req.query.page * 10 || null,
+    limit: 10
+  });
+
+  res.render("pages/patrons/all_patrons", {
+    title: "Patrons",
+    pagination: pagination({
+      page: req.query.page || 1,
+      paginationType: "patrons",
+      rowCount: patrons.count
+    }),
+    patrons: patrons.rows
   });
 });
 
-router.get("/new", (req, res) => {
-  res.render("pages/patrons/new_patron");
+router
+  .route("/patron_details/:id")
+  .get(async (req, res) => {
+    const id = req.params.id;
+    const patron = await models.patrons.findByPk(id);
+    const loans = await models.loans.findAll({
+      where: { patron_id: id },
+      include: [{ model: models.books, attributes: ["id", "title"] }]
+    });
+
+    res.render("pages/patrons/patron_detail", {
+      patron,
+      loans,
+      errors: {},
+      title: "Patron Details"
+    });
+  })
+  .post(async (req, res) => {
+    const id = req.params.id;
+    models.patrons.findByPk(id).then(instance => {
+      instance
+        .update(req.body)
+        .then(() => res.redirect("/patrons"))
+        .catch(async error => {
+          res.render("pages/patrons/patron_detail", {
+            title: "Patron Details",
+            previousValues: req.body,
+            errors: formErrorCreator(error),
+            patron: await models.patrons.findByPk(id),
+            loans: await models.loans.findAll({
+              where: { patron_id: id },
+              include: [{ model: models.books, attributes: ["id", "title"] }]
+            })
+          });
+        });
+    });
+  });
+
+router.route("/new")
+.get(async (req, res) => {
+  res.render("pages/patrons/new_patron", {
+    prevValues: {},
+    errors: {},
+    title: "New Patron"
+  });
+})
+.post(async (req, res) => {
+  models.patrons
+  .create(req.body)
+  .then(() => {
+    res.redirect("/patrons");
+  })
+  .catch(error => {
+    const prevValues = req.body;
+    res.render("pages/patrons/new_patron", {
+      title: "New Patron",
+      prevValues,
+      errors: formErrorCreator(error)
+    });
+  });
 });
 
 module.exports = router;
