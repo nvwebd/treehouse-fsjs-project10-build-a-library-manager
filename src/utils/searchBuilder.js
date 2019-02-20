0995e5200c3f67f13d03eaf0f57fffdc9a4b5fee
Nejc Vukovic <nejc.vukovic@nvwebd.com>
2019-02-20 01:06:35 +0100

    implement small searchBuilder module for search

---
+++ src/utils/searchBuilder.js
@@ -0,0 +1,29 @@
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+
+module.exports = ({ ...searchData }) => {
+  switch (searchData.searchType) {
+    case "books": {
+      return [
+        { title: { [Op.like]: `%${searchData.search}%` } },
+        { author: { [Op.like]: `%${searchData.search}%` } },
+        { genre: { [Op.like]: `%${searchData.search}%` } },
+        { first_published: { [Op.like]: `%${searchData.search}%` } }
+      ];
+    }
+
+    case "patrons": {
+      return [
+        { first_name: { [Op.like]: `%${searchData.search}%` } },
+        { last_name: { [Op.like]: `%${searchData.search}%` } },
+        { address: { [Op.like]: `%${searchData.search}%` } },
+        { email: { [Op.like]: `%${searchData.search}%` } },
+        { library_id: { [Op.like]: `%${searchData.search}%` } },
+        { zip_code: { [Op.like]: `%${searchData.search}%` } }
+      ];
+    }
+
+    default:
+      return {};
+  }
+};
